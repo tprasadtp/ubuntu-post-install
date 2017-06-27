71a94b45707f901a0fb7de268eadd71864a0d253
tprasadtp <tprasadtp@users.noreply.github.com>
2017-06-27 03:25:59 +0200

    Initial UPload-Pre-Alpha

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Ubuntu After Effects
 A bash script to setup your fresh install of ubuntu.
+Currently I am reorganizing this project so few things are broken.
---
+++ data/repos_and_deb_urls.sh
@@ -0,0 +1,33 @@
+# KEYS FOR REPOS & Key servers
+#List of variables and URLS
+# KEYS FOR REPOS & Key servers
+ARC_THEME_KEY=http://downloadvariables.opensuse.org/repositories/home:Horst3180/xUbuntu_$VERSION/Release.key
+INSYNC_KEY=https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key
+GOOGLE_KEY=https://dl.google.com/linux/linux_signing_key.pub
+SPOTIFY_KEY=BBEBDCB318AD50EC6865090613B00F1FD2C19886
+DUKTO_REPO_KEY=http://download.opensuse.org/repositories/home:colomboem/xUbuntu_$VERSION/Release.key
+SKYPE_KEY=https://repo.skype.com/data/SKYPE-GPG-KEY
+UBUNTU_KEY_SERVER=hkp://keyserver.ubuntu.com:80
+OPENPGP_KEYSERVER=http://pool.sks-keyservers.net/
+#Path for Repositories
+ARC_THEME_REPOSITORY=http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_$VERSION/
+DUKTO_REPOSITORY=http://download.opensuse.org/repositories/home:/colomboem/xUbuntu_$VERSION/
+GOOGLE_CHROME_REPOSITORY=http://dl.google.com/linux/chrome/deb/
+GOOGLE_EARTH_REPOSITORY=http://dl.google.com/linux/earth/deb/
+WHATSIE_REPO=https://dl.bintray.com/aluxian/deb
+SPOTIFY_REPO=http://repository.spotify.com
+SKYPE_REPO=https://repo.skype.com/deb
+#Path for DEBS
+SKYPE_FOR_LINUX_DEB=https://go.skype.com/skypeforlinux-64-alpha.deb
+#CUMULUS
+CUMULUS_DEB=https://github.com/kd8bny/cumulus/releases/download/v1.0.0/cumulus_1.0.0_amd64.deb
+#Google
+GOOGLE_CHROME_DEB_URL=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#Atom Editor
+ATOM_DEB_URL=https://github.com/atom/atom/releases/download/v1.10.2/atom-amd64.deb
+#Unified Remote
+UR_DEB_URL=https://www.unifiedremote.com/static/builds/server/linux-x64/740/urserver-3.4.0.740.deb
+#Visual studio Code
+VISUAL_STUDIO_CODE_URL=https://az764295.vo.msecnd.net/stable/07d663dc1bd848161edf4cd4ce30cce410d3d877/code_1.5.1-1473370243_amd64.deb
+#Teamviewer DEBS
+TEAMVIEWER_DEB_URL=https://download.teamviewer.com/download/teamviewer_i386.deb
