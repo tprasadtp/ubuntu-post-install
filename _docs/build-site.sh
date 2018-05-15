eec79a93baf3e8df922fccd6206d662e8dd095ad
tprasadtp <code@prasadt.com>
2018-05-15 18:43:19 +0200

    add build scripts for mkdocs

---
+++ _docs/build-site.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+#
+#                                        _  .
+# \,___, .___    ___    ____   ___    ___/ _/_
+# |    \ /   \  /   `  (      /   `  /   |  |
+# |    | |   ' |    |  `--.  |    | ,'   |  |
+# |`---' /     `.__/| \___.' `.__/| `___,'  \__/
+# \                                      `
+#
+
+# This is a bash script to build jekyll website and test it with htmlproofer
+# Netlify Deployments
+# Version:1.0
+# Author: Prasad Tengse
+# Licence: MIT
+# Github Repository: https://github.com/tprasadtp/prasadt
+# Requirements - Bash v4.4 and above
+#              - jekyll and bundler
+
+set -e # halt script on error
+DEPLOY_PARAM=./_docs/site/deploy-params.txt
+spacing_string="%-15s"
+#Constants
+DATE=$(date +%Y-%m-%d:%H:%M:%S)
+
+echo "---> Building Website "
+
+function gen_metadata()
+{
+  echo ">>---------------------------- Build Metadata -------------------------------------<<" >>${DEPLOY_PARAM}
+  echo "This Version of Website was Generated On ${DATE} By Netlify Build Bots." >> ${DEPLOY_PARAM}
+  printf "${spacing_string}: $BRANCH\n" "Branch" >>${DEPLOY_PARAM}
+  printf "${spacing_string}: $PULL_REQUEST\n" "Is Pull Request" >>${DEPLOY_PARAM}
+  printf "${spacing_string}: $COMMIT_REF\n" "Commit" >>${DEPLOY_PARAM}
+  printf "${spacing_string}: $CONTEXT\n" "Deploy Type" >>${DEPLOY_PARAM}
+  printf "${spacing_string}: $DEPLOY_URL\n" "Deploy URL" >>${DEPLOY_PARAM}
+  printf "${spacing_string}: $DEPLOY_PRIME_URL\n" "Prime URL" >>${DEPLOY_PARAM}
+  echo ">>--------------------------- End Build Metadata ----------------------------------<<" >>${DEPLOY_PARAM}
+  cat ${DEPLOY_PARAM}
+}
+
+function copy_static_api_files()
+{
+  echo "Copying Static Files"
+  cp -R ./api/ ./_docs/site/
+  #statements
+}
+
+function html-proofer-checks()
+{
+echo "---> Checking links with HTML Proofer"
+echo "Errors will halt the script and Deployment. Check for recently added links and avoid links which redirect. Remember If a link becomes dead [In Future] build will fail!!"
+echo "---------------------------------------------------------"
+bundle exec htmlproofer ./_docs/site --only-4xx --check-favicon --check-html
+echo "---------------------------------------------------------"
+}
+
+function jekyll_production()
+{
+  echo "---> Building Website with Production Context"
+  mkdocs build;
+  gen_metadata;
+  html-proofer-checks;
+
+}
+
+function jekyll_branch()
+{
+  echo "---> Building Website with Branch"
+  mkdocs build;
+  gen_metadata;
+  html-proofer-checks;
+}
+
+
+function usage()
+{
+  #Prints out help menu
+cat <<EOF
+Usage: netlify-deploy [OPTIONS]
+[-m --master]        [Master Deployment]
+[-b --branch]        [Branch Deployment]
+[-pr --pull-request] [Pull request deployment(Same as branch)]
+EOF
+}
+
+function install_dependencies()
+{
+  pip install mkdocs mkdocs-material
+  mkdocs --version
+  bundle install
+}
+
+
+function main()
+{
+      #check if no args
+      if [ $# -eq 0 ]; then
+              echo "------> No arguments found. See usage below."
+              usage;
+      		    exit 1;
+      fi;
+
+      install_dependencies;
+      # Process command line arguments.
+      while [ "$1" != "" ]; do
+          case ${1} in
+              -m | --master )         jekyll_production;
+                                      exit $?
+                                      ;;
+              -b | --branch )         jekyll_branch;
+                                      exit $?
+                                      ;;
+              -pr | --pull-request )  jekyll_branch;
+                                      exit $?
+                                      ;;
+              * )                     echo "Invalid arguments";
+                                      usage;
+                                      exit 1
+                                      ;;
+          esac
+      	shift
+      done
+  }
+#
+main "$@"
