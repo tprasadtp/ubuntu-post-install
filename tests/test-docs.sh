3d2302d4a885f770ed16a1c27ed6c91ac5ea8c98
tprasadtp <code@prasadt.com>
2018-07-24 14:37:37 +0200

    update test docs script

--- tests/test-docs.sh
+++ tests/test-docs.sh
@@ -31,14 +31,15 @@ sha1sum after-effects get-after-effects.sh | tee -a sha1.txt
 sha256sum after-effects get-after-effects.sh | tee -a sha256.txt
 echo "Copying Checksums"
 mkdir -p ./_site/ || echo "Failed to create dir _site"
-cp *.txt ./_site/ || echo "Failed to copy SHA Checksums"
+cp ./*.txt ./_site/ || echo "Failed to copy SHA Checksums"
 echo "Copy API Files"
 cp -R ./api/ ./_site/api/ || echo "Failed to copy config files"
 
 echo "Copying Signature file"
 if [ -f after-effects.asc ]; then
-  mkdir -p ./_site/api/gpg
-	cp ./after-effects.asc ./_site/api/gpg/after-effects
+  mkdir -p ./_site/api/gpg || echo "Failed to create dir _site"
+	cp ./after-effects.asc ./_site/api/gpg/after-effects \
+		|| echo "Failed to copy gpg files"
 fi
 echo "Copy Netlify Files"
 cp  ./netlify.toml ./_site/netlify.toml
@@ -46,9 +47,12 @@ mkdir -p ./_site/vendor
 cp ./vendor/netlify-build.sh ./_site/vendor/netlify-build.sh
 echo "Commit Info"
 true > ./_site/commit.txt
-printf "${spacing_string}: ${TRAVIS_COMMIT:0:7}\n" "SRC Commit ID"  | tee -a ./_site/commit.txt
-printf "${spacing_string}: ${TRAVIS_COMMIT_MESSAGE}\n" "SRC Message" | tee -a ./_site/commit.txt
-printf "${spacing_string}: ${TRAVIS_BRANCH}\n" "Built from" | tee -a ./_site/commit.txt
+printf "${spacing_string}: ${TRAVIS_COMMIT:0:7}\n" "SRC Commit ID"  \
+| tee -a ./_site/commit.txt
+printf "${spacing_string}: ${TRAVIS_COMMIT_MESSAGE}\n" "SRC Message" \
+| tee -a ./_site/commit.txt
+printf "${spacing_string}: ${TRAVIS_BRANCH}\n" "Built from" \
+| tee -a ./_site/commit.txt
 # Install htmlproofer
 #echo "Installing bundler dependencies"
 #bundle install
