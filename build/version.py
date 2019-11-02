f35c6842cd0bba327ad6b3891a26ce71c0007fb3
Prasad Tengse <tprasadtp@users.noreply.github.com>
2019-11-02 01:30:28 +0100

    fix netlify as it des not have 3.6

--- build/version.py
+++ build/version.py
@@ -9,10 +9,10 @@ v_input_file = Path('config/version.yml')
 
 
 if v_input_file.exists:
-    with open(v_input_file, 'r') as f:
+    with open(v_input_file.as_posix(), 'r') as f:
         yaml_data = yaml.safe_load(f)
     print(yaml_data)
-    with open(v_output_file, 'w') as jsonfile:
+    with open(v_output_file.as_posix(), 'w') as jsonfile:
         json.dump(yaml_data, jsonfile, ensure_ascii=True,
                   indent=4, sort_keys=True)
 else:
